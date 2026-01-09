b544b44fb9cd2b61bb4cf44b40fe8fa360fc9877
gpku92 <ganeshpatil8980@gmail.com>
2026-01-09 21:35:43 +0530

    added fauiled test case

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-it('renders without crashing', () => {
+it('fails intentionally to validate test pipeline', () => {
   const div = document.createElement('div');
   ReactDOM.render(<div />, div);
+
+  // Intentional failure
+  expect(true).toBe(false);
 });
